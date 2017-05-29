7909b5479c6ae976bdb2064ce965b5219de3d2b4
Tim Docker <tim@dockerz.net>
2017-05-30 09:38:34 +1000

    Use new adlc options to include the haskell runtime

--- scripts/gen-adl.sh
+++ scripts/gen-adl.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
-ADLDIR=
-
 rm -rf $ROOT/src/ADL
-
-# Need to implement a haskell backend function to output the runtime
-cp -r $HOME/personal/repos/adl/haskell/runtime/src/ADL $ROOT/src/ADL
-
-adlc haskell -O $ROOT/src --moduleprefix=ADL $ROOT/adl/config.adl
+adlc haskell -O $ROOT/src --package=ADL --rtpackage=ADL.Core --include-rt $ROOT/adl/config.adl
